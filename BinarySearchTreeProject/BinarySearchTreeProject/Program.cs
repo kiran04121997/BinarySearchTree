9764ed1e54e8916ac86766cbb68f437363fda372
kiran04121997 <bhosalekiran259@gmail.com>
2022-02-09 12:22:44 +0530

    [kiran] Create BST

--- BinarySearchTreeProject/BinarySearchTreeProject/Program.cs
+++ BinarySearchTreeProject/BinarySearchTreeProject/Program.cs
@@ -10,7 +10,13 @@ namespace BinarySearchTreeProject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("BinarySearchTree");
+            Console.WriteLine("Welcome to Binary Search Tree");
+
+            MyBinarySearchTree<int> binaryTree = new MyBinarySearchTree<int>(56);
+            binaryTree.Insert(30);
+            binaryTree.Insert(70);
+            binaryTree.Display();
+
             Console.ReadLine();
         }
     }
